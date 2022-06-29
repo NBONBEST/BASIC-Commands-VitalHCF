8748c18ed0327887b7b02f3c7d50649f07a73fe3
NBONBEST <105756377+NBONBEST@users.noreply.github.com>
2022-06-29 15:54:36 -0300

    FeedCommand.php

---
+++ FeedCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace vLiqkz\commands;
+
+use vLiqkz\Loader;
+use vLiqkz\player\Player;
+
+use pocketmine\command\CommandSender;
+use pocketmine\command\defaults\VanillaCommand;
+use pocketmine\permission\DefaultPermissions;
+use pocketmine\utils\{Config, TextFormat as TE};
+
+use pocketmine\item\{Item, ItemIds};
+use pocketmine\item\ItemFactory;
+
+class FeedCommand extends VanillaCommand {
+
+    /**
+     * FeedCommand Constructor.
+     */
+    public function __construct(){
+        parent::__construct("feed", "Can fill your food bar to 100%", "feed");
+        $this->setPermission("feed.command.use");
+    }
+
+    /**
+	 * @param CommandSender $sender
+	 * @param String $label
+	 * @param Array $args
+     * @return void
+	 */
+	public function execute(CommandSender $sender, String $label, Array $args) : void {
+	    if(!$sender->hasPermission("feed.command.use")){
+            $sender->sendMessage(TE::RED."You have not permissions to use this command");
+	        return;
+        }
+        $sender->getHungerManager()->setFood(20);
+        $sender->getHungerManager()->setSaturation(20);
+
+        $sender->sendMessage(str_replace(["&"], ["§"], Loader::getConfiguration("messages")->get("player_fill_food_correctly")));
+    }
+}
+
+?>
