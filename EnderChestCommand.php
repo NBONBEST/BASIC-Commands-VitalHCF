d5618ba2a8b3505950a8048231ffd04e82542039
NBONBEST <105756377+NBONBEST@users.noreply.github.com>
2022-06-29 04:32:59 -0300

    EnderChestCommand.PHP
    this basic command allows you to save things in it in an enderches

---
+++ EnderChestCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace vLiqkz\commands;
+
+use vLiqkz\Loader;
+use vLiqkz\player\Player;
+use libs\muqsit\invmenu\transaction\InvMenuTransactionResult;
+use vLiqkz\API\InvMenu\type\EnderChestInventory;
+
+use pocketmine\utils\TextFormat as TE;
+use pocketmine\command\CommandSender;
+use pocketmine\command\defaults\VanillaCommand;
+use pocketmine\permission\DefaultPermissions;
+use libs\muqsit\invmenu\InvMenu;
+use pocketmine\network\mcpe\protocol\BlockActorDataPacket;
+use pocketmine\network\mcpe\protocol\UpdateBlockPacket;
+use pocketmine\item\ItemFactory;
+use pocketmine\item\ItemIds;
+
+class EnderChestCommand extends VanillaCommand {
+	public function __construct(){
+		parent::__construct("enderchest", "Can open your EnderChest", "ec", ["ec", "enderchest"]);
+	}
+	public function execute(CommandSender $sender, String $label, Array $args) : void {
+		$menu = InvMenu::create("invmenu:chest");
+        $menu->getInventory()->setContents($sender->getEnderInventory()->getContents());
+		$menu->setName("EnderChest");
+        $menu->setInventoryCloseListener(function($player, $inventory) : void{
+			$player->getEnderInventory()->setContents($inventory->getContents());
+		});
+		$menu->send($sender);
+	}
+}
+
+?>
