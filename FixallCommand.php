46f088a437634141921ebcef59c249159c2408eb
NBONBEST <105756377+NBONBEST@users.noreply.github.com>
2022-06-29 05:31:20 -0300

    fixallCommand
    repair your armor using /fix all

---
+++ FixallCommand.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace vLiqkz\commands;
+
+use vLiqkz\Loader;
+use vLiqkz\player\Player;
+
+use pocketmine\item\{Item, Armor, Tool};
+
+use pocketmine\utils\TextFormat as TE;
+use pocketmine\command\CommandSender;
+use pocketmine\command\defaults\VanillaCommand;
+use pocketmine\permission\DefaultPermissions;
+
+class FixallCommand extends VanillaCommand {
+	
+	/**
+	 * FixCommand Constructor.
+	 */
+	public function __construct(){
+		parent::__construct("fixall", "FixAll For everyone!", "fixall");
+	}
+	
+	/**
+	 * @param CommandSender $sender
+	 * @param String $label
+	 * @param Array $args
+     * @return void
+	 */
+	public function execute(CommandSender $sender, String $label, Array $args) : void {
+        if(!$sender instanceof Player){
+            $sender->sendMessage('vaya?');
+        }
+		if($sender->hasPermission(DefaultPermissions::ROOT_OPERATOR)){
+            $players = "";
+			foreach($sender->getServer()->getOnlinePlayers() as $player) {
+                    $players = $player->getName().$players.", ";
+             			foreach($player->getInventory()->getContents() as $slot => $item){
+							if($item instanceof Tool||$item instanceof Armor){
+								if($item->getDamage() > 0){
+									$player->getInventory()->setItem($slot, $item->setDamage(0));
+								}
+							}
+						}
+						foreach($player->getArmorInventory()->getContents() as $slot => $item){
+							if($item instanceof Tool||$item instanceof Armor){
+								if($item->getDamage() > 0){
+									$player->getArmorInventory()->setItem($slot, $item->setDamage(0));
+							}
+						}
+					}
+         		
+    		}
+			$sender->sendMessage("Has reparado los items a estos jugadores: ", $players);
+        }
+	}
+}
+
+?>
